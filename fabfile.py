9e9a69cb7467786c2d46f0afe98a415966ab292b
exequielrafaela <exequielrafaela@gmail.com>
2016-09-16 18:31:10 -0300

    knife_conf_centos() testing

--- fabfile.py
+++ fabfile.py
@@ -664,6 +664,7 @@ Initialize knife zero on RedHat/Centos OS
 
         try:
             with cd ('/home/' + usernamek +'/my_chef_repo'):
+                sudo('pwd')
                 with open("./conf/servers/out_users_test.txt", "r") as f:
                     ServerList = [line.split()[0] for line in f]
                     for serverIp in ServerList:
