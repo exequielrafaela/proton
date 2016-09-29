0474720d3a71545675481a11f6f6905b6f661c10
exequielrafaela <exequielrafaela@gmail.com>
2016-09-29 16:56:12 -0300

    key_remove(user) added

--- fabfile.py
+++ fabfile.py
@@ -2365,13 +2365,13 @@ eg: fab -R devtest rsync_data_to_server_v2:/tmp/172.28.128.4/,/tmp/172.28.128.4/
                 if os.path.isdir(local_rsync_dir) and exists(remote_dir):
                     rsync_project(local_dir=local_rsync_dir, remote_dir=remote_dir,
                                   default_opts='-avzP --progress')
-                    local('sudo rm -rf ' + local_rsync_dir)
+                    local('sudo rm -rf ' + local_rsync_dir[-1])
                 else:
                     try:
                         sudo('mkdir -p ' + remote_dir)
                         rsync_project(local_dir=local_rsync_dir, remote_dir=remote_dir,
                                       default_opts='-avzP --progress')
-                        local('sudo rm -rf ' + local_rsync_dir)
+                        local('sudo rm -rf ' + local_rsync_dir[-1])
 
                     except SystemExit:
                         print colored('#################################################################', 'red')
