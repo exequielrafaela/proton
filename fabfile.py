aa1701415f39e76ec15c81af707369a6c3efcfd4
exequielrafaela <exequielrafaela@gmail.com>
2016-10-18 17:11:11 -0300

    password sec functions added

--- fabfile.py
+++ fabfile.py
@@ -1910,9 +1910,9 @@ eg: fab -R localhost mysql_restore_rds_to_new_db:backup-2016-10-04-16-13-10-172.
         date = strftime("%Y_%m_%d_%H_%M_%S", gmtime())
 
         try:
-            database = ""
-            #database = sudo('mysql -h ' + mysql_ip + ' -u ' + mysql_user + ' -p' + password +
-            #            ' -e "show databases;" | grep ' + db_name)
+            database = sudo('mysql -h ' + mysql_ip + ' -u ' + mysql_user + ' -p' + password +
+                        ' -e "show databases;" | grep ' + db_name)
+            print database
 
             if os.path.isfile(local_dir + mysqldump_fname):
                 if database != "":
