5b4b2795b634e7febfaec976d661624b5f5d995f
exequielrafaela <exequielrafaela@gmail.com>
2016-12-12 10:50:28 -0300

    user_remove_from_group_centos(usernamelist, group) among other user mgmt funcs added

--- fabfile.py
+++ fabfile.py
@@ -368,9 +368,9 @@ Install/Upgrade an RedHat/Centos yum based linux package
             print colored('############################################################################', 'blue')
 
 
-def user_add_centos(usernamec):
+def user_add_centos_sudo(usernamec):
     """
-Add a user in RedHat/Centos based OS
+Add a user in RedHat/Centos based OS in wheel group (sudo)
     :param usernamec: "username" to add
     """
     with settings(warn_only=True):
@@ -405,6 +405,42 @@ Add a user in RedHat/Centos based OS
             print colored('######################################################', 'green')
 
 
+def user_add_centos(usernamec):
+    """
+Add a user in RedHat/Centos based OS
+    :param usernamec: "username" to add
+    """
+    with settings(warn_only=True):
+        # usernamep = prompt("Which USERNAME you like to CREATE & PUSH KEYS?")
+        # user_exists = sudo('cat /etc/passwd | grep '+usernamep)
+        # user_exists =sudo('grep "^'+usernamep+':" /etc/passwd')
+        # #user_exists = sudo('cut -d: -f1 /etc/passwd | grep ' + usernamep)
+        # print colored(user_exists, 'green')
+        # print(env.host_string)
+        # sudo('uname -a')
+
+        try:
+            # if(user_exists != ""):
+            user_true = sudo('cut -d: -f1 /etc/passwd | grep ' + usernamec)
+            if user_true != "":
+                print colored('##############################', 'green')
+                print colored('"' + usernamec + '" already exists', 'green')
+                print colored('##############################', 'green')
+
+            else:
+                print colored('#################################', 'green')
+                print colored('"' + usernamec + '" doesnt exists', 'green')
+                print colored('WILL BE CREATED', 'green')
+                print colored('##################################', 'green')
+                sudo('useradd ' + usernamec + ' -m -d /home/' + usernamec)
+
+        except SystemExit:
+            # else:
+            print colored('######################################################', 'green')
+            print colored('"' + usernamec + '" couldnt be created for some reason', 'green')
+            print colored('######################################################', 'green')
+
+
 def user_add_ubuntu(usernamec):
     """
 Add a user in Debian/Ubuntu based OS
@@ -431,6 +467,38 @@ Add a user in Debian/Ubuntu based OS
             # else:
             print colored('######################################################', 'green')
             print colored('"' + usernamec + '" couldnt be created for some reason', 'green')
+            print colored('######################################################', 'green')
+
+
+def user_remove_from_group_centos(usernamelist, group):
+    """
+Remove a user from a certain group in RedHat/Centos based OS
+    :param usernamec: "username" list to add
+    :param group: "group" where the user is going to be romoved
+
+eg: fab -f fabfile.py user_remove_from_group_centos:'tom;dick;harry',wheel
+    """
+    with settings(warn_only=True):
+        try:
+            if isinstance(usernamelist, basestring):
+                usernamelist = usernamelist.split(";")
+            for usern in usernamelist:
+                user_true = sudo('cut -d: -f1 /etc/passwd | grep ' + usern)
+                if user_true != "":
+                    print colored('##################################################', 'green')
+                    print colored('"' + usern + 'will be removed from gorup '+ group, 'green')
+                    print colored('##################################################', 'green')
+                    sudo('gpasswd -d ' + usern + ' ' +group)
+                else:
+                    print colored('#################################', 'green')
+                    print colored('"' + usern + '" doesnt exists', 'green')
+                    print colored('NOT possible to remove it', 'green')
+                    print colored('##################################', 'green')
+
+        except SystemExit:
+            # else:
+            print colored('######################################################', 'green')
+            print colored('"' + usernamelist + '" couldnt be removed for some reason', 'green')
             print colored('######################################################', 'green')
 
 
@@ -737,6 +805,7 @@ Test SSH (authorized_keys) in the host
         print colored('###################################################', 'red')
         print colored(usernamet + ' FAIL! IP:' + env.host_string, 'red')
         print colored('###################################################', 'red')
+
 
 def ruby_install_centos():
     """
@@ -2322,6 +2391,7 @@ Logrotate installation in Ubuntu 14.04.
         sudo('chown 0:0 /etc/logrotate.d/squid3')
         sudo('chown 0:0 /etc/logrotate.d/apache2')
         sudo('chown 0:0 /etc/logrotate.d/postfix')
+
 
 def install_munin_master_ubuntu_14():
     """
