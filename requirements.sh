f859120abcdad9a4ecbe9fa3aa1043e67ccbebf4
exequielrafaela <exequielrafaela@gmail.com>
2017-03-14 14:17:33 -0300

    adding pip req packages

--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,7 @@
 set -e
 
 YUM_PACKAGE_NAME="python python-devl python-pip openssl-devel"
-DEB_PACKAGE_NAME="python2.7 python-dev python-pip libssl-dev build-essential libffi-dev libgit2 libgit2-dev"
+DEB_PACKAGE_NAME="python2.7 python-dev python-pip libssl-dev build-essential libffi-dev libgit2-0 libgit2-dev"
 
  if cat /etc/*release | grep ^NAME | grep CentOS; then
     echo "====================================="
