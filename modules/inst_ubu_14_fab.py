533331f07dd6b32fb57384c8d9437a0f74fa101f
exequielrafaela <exequielrafaela@gmail.com>
2017-03-15 12:01:43 -0300

    adding ubuntu java node maven installation

--- modules/inst_ubu_14_fab.py
+++ modules/inst_ubu_14_fab.py
@@ -506,7 +506,7 @@ Install Oracle Java8
         print "CURRENT JAVA VER: " + java_version
         if "1.8.0" not in java_version:
             # Set up the repository.
-            sudo('add-apt-repository ppa:webupd8team/java')
+            sudo('add-apt-repository ppa:webupd8team/java -y')
             sudo('apt-get update')
             run('echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections')
             sudo('apt-get install -y oracle-java8-installer')
